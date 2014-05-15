47f3a49a6da0c23bdb037431bfe85b00c73d85c5
Gregor Aisch <contact@vis4.net>
2014-05-14 22:14:22 -0400

    added init function

--- plugin.php
+++ plugin.php
@@ -1,7 +1,13 @@
 <?php
 
 class DatawrapperPlugin_HelloWorld extends DatawrapperPlugin {
-    
+
+    public function init() {
+        // whatever we do here will be executed on each request
+        // Try to uncomment the following line to break Datawrapper
+        //print "Hello world"; exit;
+    }
+
 }
 
 ?>
