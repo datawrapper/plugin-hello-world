d296987867be220a5e9049109db8508e579d8851
Gregor Aisch <contact@vis4.net>
2014-05-14 22:20:07 -0400

    add simple controller

--- plugin.php
+++ plugin.php
@@ -6,6 +6,25 @@ class DatawrapperPlugin_HelloWorld extends DatawrapperPlugin {
         // whatever we do here will be executed on each request
         // Try to uncomment the following line to break Datawrapper
         //print "Hello world"; exit;
+
+        // let's register a new page within Datawrapper
+        DatawrapperHooks::register(
+            DatawrapperHooks::GET_PLUGIN_CONTROLLER,
+            array($this, 'helloWorld')
+        );
+    }
+
+    /*
+     * controller function
+     *
+     * @param app  Slim Framework instance
+     *   see http://docs.slimframework.com/#Routing-Overview
+     */
+    public function helloWorld($app) {
+        // register controller under http://datawrapper/hello-world
+        $app->get('/hello-world', function() use ($app) {
+            echo "Hello World";
+        });
     }
 
 }
